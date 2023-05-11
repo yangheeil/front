594c7253f3934cc64a8d1c8106022a96ae819b20
yangharry <myid7771@naver.com>
2023-05-12 03:08:13 +0900

    add dist files

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/front',
       // analyze: true,
       // env: {},
       // rawDefine: {}
