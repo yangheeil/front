7b2c80d8b9320f570978ba123d0fc954fa8f2455
yangharry <myid7771@naver.com>
2023-05-12 03:31:08 +0900

    add docs

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      // distDir,
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
