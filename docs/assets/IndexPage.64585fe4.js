fda971d60f90ec2e0679682106b4d72b83a027b7
yangharry <myid7771@naver.com>
2023-05-14 03:27:56 +0900

    add drawer toolbar under 600px

--- docs/assets/IndexPage.64585fe4.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.bb5b6d98.js";import{$ as a,E as r,F as t,G as c,J as o}from"./index.756269ae.js";import"./render.1d422ee9.js";const s={};function f(n,_){return r(),t(e,{class:"flex flex-center"},{default:c(()=>[o(" \uAC8C\uC2DC\uD310\uB4E4 \uBCF4\uC5EC\uC8FC\uAE30 ")]),_:1})}var i=a(s,[["render",f]]);export{i as default};
